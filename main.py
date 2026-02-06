e1b4a5ee157061a7a038284e3c460e9b17af42b5
Oleksandr Rachynskyi <olek.rachynskyi@gmail.com>
2026-02-06 21:49:48 +0100

    homework_1_task

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-print("Hello world")    
+from datetime import datetime
 
-print("Hello Git")  
-print("Olek2002")
-print("olek")
+def get_days_from_today(date):
+    # 1. Перетворюємо рядок у об'єкт datetime
+    # Формат %Y-%m-%d — це якраз РРРР-ММ-ДД
+    target_date = datetime.strptime(date, "%Y-%m-%d")
+    
+    # 2. Отримуємо поточну дату
+    today = datetime.today()
+    
+    # 3. Рахуємо різницю
+    difference = today - target_date
+    
+    # 4. Повертаємо кількість днів як ціле число (.days)
+    return difference.days
+
+# Перевірка:
+print(get_days_from_today("2020-10-09")) # Виведе кількість днів, що минули
+print(get_days_from_today("2026-10-09")) # Виведе від'ємне число, бо дата в майбутньому
